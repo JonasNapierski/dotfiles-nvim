f65fe49564813895a49847f7d4b702de77bd53a4
Jonas Napierski <pudeljoni@gmail.com>
2024-08-13 10:05:37 +0200

    add swagger preview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,28 +1,37 @@
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'nvim-telescope/telescope.nvim'
-	use 'nvim-lua/plenary.nvim'
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  use  {
-    "Ilyasyoy/coredor.nvim",
-  }
-  use  {"epwalsh/obsidian.nvim"}
-  use "godlygeek/tabular"
-  use "preservim/vim-markdown"
-  use {
-  "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim",
+    use 'wbthomason/packer.nvim'
+    use 'nvim-telescope/telescope.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use {
+        "catppuccin/nvim",
+        as = "catppuccin" }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {
+            'nvim-tree/nvim-web-devicons',
+            opt = true
+        }
     }
-  }
-  use {'neoclide/coc.nvim', branch = 'release'}
+    use    {
+        "Ilyasyoy/coredor.nvim",
+    }
+    use    {"epwalsh/obsidian.nvim"}
+    use "godlygeek/tabular"
+    use "preservim/vim-markdown"
+    use {
+    "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+            "3rd/image.nvim",
+        }
+    }
+    use {'neoclide/coc.nvim', branch = 'release'}
+    use {
+            "vinnymeller/swagger-preview.nvim",
+            run = "npm install -g swagger-ui-watcher",
+    }
 end)
 
