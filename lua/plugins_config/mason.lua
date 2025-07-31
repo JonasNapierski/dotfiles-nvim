622f9fd729671f6dd62457c14f4071ad792435a3
Jonas Napierski <30770965+JonasNapierski@users.noreply.github.com>
2025-07-31 21:24:51 +0200

    fix basedpyright and ts_ls

--- lua/plugins_config/mason.lua
+++ lua/plugins_config/mason.lua
@@ -16,10 +16,23 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<c-s>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format({ async = true }) end, opts)
 
-    -- open ups completion menu
+
+    vim.keymap.set('i', '<Tab>', function()
+        return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
+    end, { expr = true })
+
+    vim.keymap.set('i', '<S-Tab>', function()
+        return vim.fn.pumvisible() == 1 and "<C-p>" or "<S-Tab>"
+    end, { expr = true })
+
+    vim.keymap.set('i', '<CR>', function()
+        return vim.fn.pumvisible() == 1 and "<C-y>" or "<CR>"
+    end, { expr = true })
+
     vim.keymap.set('i', '<c-space>', function()
         vim.lsp.completion.get()
     end)
+
 end
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -33,6 +46,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
+
 
 -- Use `vim.lsp.config` to configure servers BEFORE mason-lspconfig sets them up
 vim.lsp.config.clangd = {
@@ -55,6 +69,7 @@ vim.lsp.config.lua_ls = {
         }
     }
 }
+
 vim.lsp.config.cmake = {
     on_attach = on_attach,
     capabilities = capabilities,
@@ -79,14 +94,11 @@ vim.lsp.config.omnisharp = {
     on_attach = on_attach,
     capabilities = capabilities,
 }
-vim.lsp.config.tsserver = {
-    on_attach = function(client, bufnr)
-        client.server_capabilities.documentFormattingProvider = false
-        on_attach(client, bufnr)
-    end,
+vim.lsp.config.ts_ls = {
+    on_attach = on_attach,
     capabilities = capabilities,
 }
-vim.lsp.basedpyright = {
+vim.lsp.config.basedpyright = {
     on_attach = on_attach,
     capabilities = capabilities,
 }
